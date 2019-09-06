24e9d4d9b2461d1346b10eeabfa90b860b523a83
EinarK2 <einarkarlp@gmail.com>
2019-09-06 08:17:29 +0000

    bruh

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 from flask import Flask
-from datetime import datetime
-import re
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return """
+    <div style="display:flex;">
+	    <p style="padding-right:10px;"><a href="/">About</a></p>
+	    <p style="padding-right:10px;"><a href="/pictures">Pictures</a></p>
+	    <p style="padding-right:10px;"><a href="/biography">Biography</a></p>
+	</div>
+    """
 
-@app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+@app.route("/pictures")
+def pictures():
+	return("<a href='/'>pictures</a>")
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+@app.route("/biography")
+def biography():
+	return("<a href='/'>biography</a>")
